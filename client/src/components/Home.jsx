a614ed2eddd36f26f53bd0f05939a5f72c8110c8
Elijah Barrios <elijahbarrios@gmail.com>
2023-01-05 20:50:28 -0500

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import Image from 'react-bootstrap/Image'
 const Home = () => {
    return (
       <>
-         <Image fluid src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/backgrounds/dpc_header_event_summer22.jpg"/>
+         <Image fluid src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/dpc/headers/dpc_header_event_winter23.jpg"/>
          <div className="home mt-3 mx-5">
             <h1 className="">DPC Stat Tracker</h1>
             <p className="text-gray">Select a region above to start</p>
