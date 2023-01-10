2bdc26caf18fe6759ae0b2e2fcb17dd478de4a7d
Elijah Barrios <elijahbarrios@gmail.com>
2023-01-10 14:32:27 -0500

    Testing hero count

--- client/src/components/Tables/Player/PlayerHeroes.jsx
+++ client/src/components/Tables/Player/PlayerHeroes.jsx
@@ -6,7 +6,10 @@ const PlayerHeroes = ({heroes}) => {
     <div>
        {heroes.map((hero, key) => {
           return (
-             <Image key={key} src={"https://api.opendota.com"+HeroIcons[hero[0]].icon} />{hero[1]}
+            <>
+             <Image key={key} src={"https://api.opendota.com"+HeroIcons[hero[0]].icon} />
+             {hero[1]}
+            </>
           )
        })}
     </div>
