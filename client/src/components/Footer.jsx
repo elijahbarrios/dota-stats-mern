da4a568de3a87df607d30cc0ccb20248a2f2b44c
Elijah Barrios <elijahbarrios@gmail.com>
2023-01-13 13:52:45 -0500

    Footer fixes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,31 +10,25 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col>
-                    <Card border="light">
-                        <Card.Body>
-                            <Card.Title>Contact</Card.Title>
-                            <Card.Text>
-                                <ion-icon name="logo-discord" size="large" style={{ color: '#5865F2'}}></ion-icon>
-                                <a href="https://discordapp.com/users/77522609985564672" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>mtk357#0491</a>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Stack>
+                        <h5>Contact</h5>
+                        <div>
+                            <ion-icon name="logo-discord" size="large" style={{ color: '#5865F2'}}></ion-icon>
+                            <a href="https://discordapp.com/users/77522609985564672" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>mtk357#0491</a>
+                        </div>
+                    </Stack>
                 </Col>
                 <Col></Col>
                 <Col>
-                    <Card border="light">
-                        <Card.Body>
-                            <Card.Title>Built using</Card.Title>
-                        </Card.Body>
-                        <Stack>
-                            <a href="https://www.opendota.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>OpenDota API</a>
-                            <a href="https://reactjs.org/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>React</a>
-                            <a href="https://react-bootstrap.github.io/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>React-Bootstrap</a>
-                            <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>Node.js</a>
-                            <a href="https://expressjs.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>Express</a>
-                            <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>MongoDB</a>
-                        </Stack>
-                    </Card>
+                    <Stack>
+                        <h5>Built using</h5>
+                        <a href="https://www.opendota.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>OpenDota API</a>
+                        <a href="https://reactjs.org/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>React</a>
+                        <a href="https://react-bootstrap.github.io/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>React-Bootstrap</a>
+                        <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>Node.js</a>
+                        <a href="https://expressjs.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>Express</a>
+                        <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>MongoDB</a>
+                    </Stack>
                 </Col>
             </Row>
         </Container>
