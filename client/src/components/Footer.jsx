dfe6ade8427078508c025fd83040e518277096a1
Elijah Barrios <elijahbarrios@gmail.com>
2023-01-13 14:17:00 -0500

    Percentage fix

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,6 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Stack from "react-bootstrap/Stack"
-import Card from "react-bootstrap/Card"
 
 const Footer = () => {
   return (
