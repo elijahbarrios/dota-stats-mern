fbc3ef5aa49f6f362d94773bb8c2113fad969ef8
Elijah Barrios <elijahbarrios@gmail.com>
2023-01-13 20:26:12 -0500

    Winrate NaN bug

--- client/src/components/Tables/Draft/DraftTable.jsx
+++ client/src/components/Tables/Draft/DraftTable.jsx
@@ -16,7 +16,7 @@ const DraftTable = ({loadingDraft, selectedStatGroup, draftData}) => {
       },
       {
          name: "Winrate",
-         selector: hero => hero.wins,
+         selector: hero => hero.pickCount > 0 ? (hero.wins / hero.pickCount) : 0,
          sortable: true,
          cell: hero => <HeroWinrate wins={hero.wins} numPicks={hero.pickCount} />,
          compact: true
